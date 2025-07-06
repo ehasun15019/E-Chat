5cd3e73a31bf2b1928de87efc900bdaab24f1686
Your Full Name <ehasun0909@gamil.com>
2025-07-06 10:22:06 +0600

    06July-2025

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -5,7 +5,7 @@ import assets from "../../assets/assets";
 const Login = () => {
   return (
     <div className="login">
-      <img src={assets.logo_big} alt="logo" />
+      <img src={assets.logo_big} alt="logo" className="logo" />
 
       <form className="login-from">
         <h2>Sign Up</h2>
@@ -35,6 +35,12 @@ const Login = () => {
         <div className="login-term">
           <input type="checkbox" name="" id="" />
           <p>Agree to the terms of use & privacy policy</p>
+        </div>
+
+        <div className="login-forgot">
+          <p className="login-toggle">
+            Already have an account <span>click here</span>
+          </p>
         </div>
       </form>
     </div>
