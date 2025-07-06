9b3d8a6da3e721c971d2f74ac3daec5a0c18c477
Your Full Name <ehasun0909@gamil.com>
2025-07-07 02:27:35 +0600

    07july-2025

--- src/components/Leftsidebar/Leftsidebar.jsx
+++ src/components/Leftsidebar/Leftsidebar.jsx
@@ -8,6 +8,26 @@ const Leftsidebar = () => {
       <div className="ls-top">
         <div className="ls-nav">
           <img src={assets.logo} className="logo" alt="logo" />
+
+          <div className="menu">
+            <img src={assets.menu_icon} alt="menu-icon" />
+          </div>
+        </div>
+
+        <div className="ls-search">
+          <img src={assets.search_icon} alt="search icon" />
+          <input type="text" placeholder="Search here" />
+        </div>
+      </div>
+
+      <div className="ls-list">
+        <div className="friends">
+          <img src={assets.profile_img} alt="profile" />
+
+          <div>
+            <p>Richard Sanford</p>
+            <span>Hello, How are you?</span>
+          </div>
         </div>
       </div>
     </div>
