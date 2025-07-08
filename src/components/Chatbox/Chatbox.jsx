126f175d537b8cd76c4a5983b09c9f7cc8e7ed80
Your Full Name <ehasun0909@gamil.com>
2025-07-08 14:24:32 +0600

    Chatbox.jsx Update

--- src/components/Chatbox/Chatbox.jsx
+++ src/components/Chatbox/Chatbox.jsx
@@ -15,6 +15,39 @@ const Chatbox = () => {
         <img src={assets.help_icon} className="help" alt="help" />
       </div>
 
+      <div className="chat-message">
+        <div className="s-msg">
+          <p className="msg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+
+          <div>
+            <img src={assets.profile_img} alt="profile img" />
+            <p>2:30pm</p>
+          </div>
+        </div>
+
+        <div className="s-msg">
+          <img className="msg-img" src={assets.pic1} alt="random image" />
+
+          <div>
+            <img src={assets.profile_img} alt="profile img" />
+            <p>2:30pm</p>
+          </div>
+        </div>
+
+        <div className="r-msg">
+          <p className="msg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+
+          <div>
+            <img src={assets.profile_img} alt="profile img" />
+            <p>2:30pm</p>
+          </div>
+        </div>
+      </div>
+
       <div className="chat-input">
         <input type="text" placeholder="send a message" />
         <input type="file" id="image" accept="image/png, image/jpg" hidden />
