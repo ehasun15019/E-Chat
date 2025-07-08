a26941947afa432fb1d1ae87957005720733d2b8
Your Full Name <ehasun0909@gamil.com>
2025-07-09 01:21:14 +0600

    09july(2025) profileUpdated

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import "./ProfileUpdate.css";
+import assets from "../../assets/assets";
 
 const ProfileUpdate = () => {
   return (
-    <div>
-      <h2>ProfileUpdate</h2>
+    <div className="profile">
+      <div className="profile-container">
+        <form action="">
+          <h3>Profile Details</h3>
+          <label htmlFor="avatar">
+            <input type="file" id="avatar" accept=".png, .jpg, .jpeg" hidden />
+            <img src={assets.avatar_icon} alt="avatar" />
+            upload profile image
+          </label>
+
+          <input type="text" placeholder="Your Name" required />
+
+          <textarea placeholder="Write Profile bio" required></textarea>
+          <button type="submit">Save</button>
+        </form>
+
+        <img src={assets.logo_icon} className="profile-pic" alt="logo" />
+      </div>
     </div>
   );
 };
